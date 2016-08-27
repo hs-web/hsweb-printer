2c1c4a866be91a0081509ab7c3c0c2650e4f18a1
熊闯 <xiongchuang@liziba.com>
2016-08-27 15:53:19 +0800

    修改获取打印列表

--- src/main/java/org/hsweb/printer/server/handler/PrintersHandler.java
+++ src/main/java/org/hsweb/printer/server/handler/PrintersHandler.java
@@ -38,25 +38,4 @@ public class PrintersHandler implements HttpHandler {
         outputStream.write(data);
         httpExchange.close();
     }
-
-    class Pnv{
-        private String name;
-        private String port;
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getPort() {
-            return port;
-        }
-
-        public void setPort(String port) {
-            this.port = port;
-        }
-    }
 }
