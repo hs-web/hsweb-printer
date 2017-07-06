6e821a65df1e3ee107fdc9333e2df43350846786
熊闯 <xiongchuang@liziba.com>
2017-07-06 22:13:06 +0800

    增加定位打印

--- src/main/java/org/hsweb/printer/utils/printable/positionprint/simple/component/PositionSimpleComponentImage.java
+++ src/main/java/org/hsweb/printer/utils/printable/positionprint/simple/component/PositionSimpleComponentImage.java
@@ -14,7 +14,12 @@ package org.hsweb.printer.utils.printable.positionprint.simple.component;
 import org.hsweb.printer.dtos.PositionSimplePrintDTO;
 import org.hsweb.printer.utils.printable.positionprint.PositionPrintUnit;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Base64;
 
 /**
  * Created by xiong on 2017-07-06.
@@ -30,13 +35,24 @@ public class PositionSimpleComponentImage implements PositionSimpleComponent {
     }
     @Override
     public void print(int pageIndex, Graphics graphics, double xpadding, double ypadding) {
-
-            graphics.drawImage(null,
-                    (int) PositionPrintUnit.parsingUnit(positionSimplePrintDTO.getX()),
+        try {
+            Image image=null;
+            if( positionSimplePrintDTO.getContext().indexOf("http://")==0|| positionSimplePrintDTO.getContext().indexOf("https://")==0){
+                image=ImageIO.read(new URL(positionSimplePrintDTO.getContext()));
+            }else {
+                String[] split = positionSimplePrintDTO.getContext().split(",");
+                byte[] decode = Base64.getDecoder().decode(split[split.length-1]);
+                image=ImageIO.read(new ByteArrayInputStream(decode));
+            }
+            graphics.drawImage(image,
+                (int) PositionPrintUnit.parsingUnit(positionSimplePrintDTO.getX()),
                     (int)(PositionPrintUnit.parsingUnit(positionSimplePrintDTO.getY())-pageIndex*height),
                     (int)(PositionPrintUnit.parsingUnit(positionSimplePrintDTO.getWidth())),
                     (int)(PositionPrintUnit.parsingUnit(positionSimplePrintDTO.getHeight())),
                     null
             );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
